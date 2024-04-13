7882b955833aff30a6561b906144bac744fdfaec
ThunderE75 <74486623+ThunderE75@users.noreply.github.com>
2024-04-13 17:02:27 +0530

    updated 001

--- A-Array/001 - Reverse an Array/code.java
+++ A-Array/001 - Reverse an Array/code.java
@@ -1,36 +1,28 @@
-/**
- * code
- */
 public class code {
-    public class GFG { 
-        /* Function to reverse arr[] from start to end*/
-        static void reverseArray(int arr[], int start, int end) { 
-            int temp; 
-            while (start < end) { 
-                temp = arr[start]; 
-                arr[start] = arr[end]; 
-                arr[end] = temp; 
-                start++; 
-                end--; 
-            } 
-        } 
-    
-        /* Utility that prints out an array on a line */
-        static void printArray(int arr[], int size) { 
-            for (int i = 0; i < size; i++) 
-                System.out.print(arr[i] + " "); 
-            System.out.println(); 
-        } 
-    
-        // Driver code 
-        public static void main(String args[]) { 
-            int arr[] = {1, 2, 3, 4, 5, 6}; 
-            printArray(arr, 6); 
-            reverseArray(arr, 0, 5); 
-            System.out.print("Reversed array is \n"); 
-            printArray(arr, 6); 
-        } 
+    /* Function to reverse arr[] from start to end */
+    static void reverseArray(int arr[]) {
+        int temp, start = 0, end = arr.length-1;
+        while (start < end) {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
-    
-    
+    /* Utility that prints out an array on a line */
+    static void printArray(int arr[], String msg) {
+        int size = arr.length;
+        System.out.print(msg + ": [ ");
+        for (int i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("]");
+    }
+    public static void main(String args[]) {
+        System.out.print("\033[H\033[2J");  // Cleans the Windows Terminal
+        int arr[] = { 1, 2, 3, 4, 5, 6 };
+        printArray(arr, "Original Array");
+        reverseArray(arr);
+        printArray(arr, "Reversed Array");
+    }
 }
